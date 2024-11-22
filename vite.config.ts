c3fb54bf1697e3625739d167749b65bcb5394899
Bosui <rbutauskas@gmail.com>
2024-11-22 07:18:10 +0200

    Projekto Struktura

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+// vite.config.ts
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
